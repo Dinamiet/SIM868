fa8731945e12a676aac639af15e3bc9dd9b43fdc
Dinamiet <iceman.sm39@gmail.com>
2025-11-07 18:57:19 +0200

    Add more GSM commands and parsing

--- include/sim868_gsm.h
+++ include/sim868_gsm.h
@@ -4,11 +4,14 @@
 #include "atterminal.h"
 
 #include <stdbool.h>
+#include <stdint.h>
 
 typedef enum _SIM868GSMResponse_
 {
-	SIM868_GSM_FUNC = 0x3C05AEFF, // CFUN
-	SIM868_GSM_PIN  = 0x754EDBE9, // CPIN
+	SIM868_GSM_FUNC           = 0x3C05AEFF, // CFUN
+	SIM868_GSM_PIN            = 0x754EDBE9, // CPIN
+	SIM868_GSM_NETWORKCHANGE  = 0xC8DDE4F3, // CREG
+	SIM868_GSM_SIGNAL_QUALITY = 0x78219D86, // CSQ
 } SIM868GSMResponse;
 
 typedef enum _SIM868GSMFunctionality_
@@ -18,7 +21,45 @@ typedef enum _SIM868GSMFunctionality_
 	SIM868_GSM_FUNC_NO_RF   = 4,
 } SIM868GSMFunctionality;
 
+typedef enum _SIM868GSMNetworkNotify_
+{
+	SIM868_GSM_NETWORK_NOTIFY_NO,
+	SIM868_GSM_NETWORK_NOTIFY_REGISTRATION,
+	SIM868_GSM_NETWORK_NOTIFY_LOCATION,
+} SIM868GSMNetworkNotify;
+
+typedef enum _SIM868NetworkStat_
+{
+	SIM868_GSM_NETWORK_STATUS_NOT_REGISTERED,
+	SIM868_GSM_NETWORK_STATUS_REGISTERED,
+	SIM868_GSM_NETWORK_STATUS_SEARCHING,
+	SIM868_GSM_NETWORK_STATUS_DENIED,
+	SIM868_GSM_NETWORK_STATUS_UNKNOWN,
+	SIM868_GSM_NETWORK_STATUS_ROAMING,
+} SIM868GSMNetworkStat;
+
+typedef struct _SIM868GSMNetworkInfo_
+{
+	SIM868GSMNetworkStat Status;
+	uint16_t             LocationCode;
+	uint16_t             CellID;
+} SIM868GSMNetworkInfo;
+
+typedef struct _SIM868GSMSignalInfo_
+{
+	uint8_t RSSI;
+	uint8_t BER;
+} SIM868GSMSignalInfo;
+
 void SIM868_GSM_Functionality(ATTerminal* at, SIM868GSMFunctionality func);
 void SIM868_GSM_EnterPin(ATTerminal* at, char* pin);
+void SIM868_GSM_SignalCheck(ATTerminal* at);
+void SIM868_GSM_NetworkRegistrationStatus(ATTerminal* at, SIM868GSMNetworkNotify notify);
+void SIM868_GSM_SetAPN(ATTerminal* at, char* apn, char* user, char* pwd);
+void SIM868_GSM_ConnectData(ATTerminal* at);
+void SIM868_GSM_IPAddress(ATTerminal* at);
+
+SIM868GSMNetworkInfo SIM868_GSM_ParseNetworkInfo(char* info);
+SIM868GSMSignalInfo  SIM868_GSM_ParseSignalInfo(char* info);
 
 #endif
--- src/gsm.c
+++ src/gsm.c
@@ -1,8 +1,9 @@
+#include "defines.h"
 #include "sim868_gsm.h"
 
 #include <stdio.h>
-
-#define MAX_COMMAND_SIZE 16
+#include <stdlib.h>
+#include <string.h>
 
 void SIM868_GSM_Functionality(ATTerminal* at, SIM868GSMFunctionality func)
 {
@@ -16,4 +17,76 @@ void SIM868_GSM_EnterPin(ATTerminal* at, char* pin)
 	char command[MAX_COMMAND_SIZE];
 	sprintf(command, "AT+CPIN=%s", pin);
 	ATTerminal_SendCommand(at, command);
+}
+
+void SIM868_GSM_SignalCheck(ATTerminal* at) { ATTerminal_SendCommand(at, "AT+CSQ"); }
+
+void SIM868_GSM_NetworkRegistrationStatus(ATTerminal* at, SIM868GSMNetworkNotify notify)
+{
+	char command[MAX_COMMAND_SIZE];
+	sprintf(command, "AT+CGREG=%d", notify);
+	ATTerminal_SendCommand(at, command);
+}
+
+void SIM868_GSM_SetAPN(ATTerminal* at, char* apn, char* user, char* pass)
+{
+	char command[MAX_COMMAND_SIZE];
+	if (pass)
+		sprintf(command, "AT+CSTT=\"%s\",\"%s\",\"%s\"", apn, user, pass);
+	else if (user)
+		sprintf(command, "AT+CSTT=\"%s\",\"%s\"", apn, user);
+	else
+		sprintf(command, "AT+CSTT=\"%s\"", apn);
+
+	ATTerminal_SendCommand(at, command);
+}
+
+void SIM868_GSM_ConnectData(ATTerminal* at) { ATTerminal_SendCommand(at, "AT+CIICR"); }
+
+void SIM868_GSM_IPAddress(ATTerminal* at) { ATTerminal_SendCommand(at, "AT+CIFSR"); }
+
+SIM868GSMNetworkInfo SIM868_GSM_ParseNetworkInfo(char* info)
+{
+	SIM868GSMNetworkInfo networkData = {0};
+
+	char* rest  = info;
+	char* token = strsep(&rest, ",");
+
+	// Stat field
+	if (token)
+		networkData.Status = atoi(token);
+
+	// Location Code field
+	token = strsep(&rest, ",");
+	strsep(&token, "\"");
+	token = strsep(&token, "\"");
+	if (token)
+		networkData.LocationCode = strtoul(token, NULL, 16);
+
+	// Cell ID field
+	token = strsep(&rest, ",");
+	strsep(&token, "\"");
+	token = strsep(&token, "\"");
+	if (token)
+		networkData.CellID = strtoul(token, NULL, 16);
+
+	return networkData;
+}
+
+SIM868GSMSignalInfo SIM868_GSM_ParseSignalInfo(char* info)
+{
+	SIM868GSMSignalInfo signalInfo = {0};
+
+	char* rest  = info;
+	char* token = strsep(&rest, ",");
+
+	// RSSI field
+	if (token)
+		signalInfo.RSSI = atoi(token);
+
+	token = strsep(&rest, ",");
+	if (token)
+		signalInfo.BER = atoi(token);
+
+	return signalInfo;
 }
