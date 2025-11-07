59de042cf26d7c1ef77272921762161aed02631c
Dinamiet <iceman.sm39@gmail.com>
2025-11-07 16:18:57 +0200

    Call and SMS headers

---
+++ include/sim868_sms.h
@@ -0,0 +1,11 @@
+#ifndef _SIM868_SMS_H_
+#define _SIM868_SMS_H_
+
+#include "atterminal.h"
+
+typedef enum _SIM868SMSResponse_
+{
+	SIM868_SMS_READY = 0xEE605F1D, // SMS Ready
+} SIM868SMSResponse;
+
+#endif
